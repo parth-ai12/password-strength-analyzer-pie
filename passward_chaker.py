4c5d6aff8add0874575203f9d63ed372f5839d9c
Parth Rawat <surbhitrawat70@gmail.com>
2026-03-28 06:02:32 +0530

    Update passward_chaker.py

--- passward_chaker.py
+++ passward_chaker.py
@@ -9,7 +9,6 @@ password = input("Enter your password: ")
 
 score = 0
 
-# Checks
 if len(password) >= 8:
     score += 1
 
@@ -22,11 +21,9 @@ if re.search("[0-9]", password):
 if re.search("[!@#$%^&*]", password):
     score += 1
 
-# Show score
 print(Fore.YELLOW + f"Strength Score: {score}/4")
 print(Style.RESET_ALL)
 
-# Result
 if score <= 1:
     print(Fore.RED + "Strength: Weak ❌")
 elif score <= 3:
@@ -36,18 +33,14 @@ else:
 
 print(Style.RESET_ALL)
 
-# 📊 Pie Chart Data
 labels = ['Passed', 'Failed']
 values = [score, 4 - score]
 
-# Create chart
 plt.pie(values, labels=labels, autopct='%1.0f%%')
 plt.title("Password Strength Analysis")
 
-# ✅ Save image
 plt.savefig("password_strength_chart.png")
 
-# Show chart
 plt.show()
 
 print("📁 Chart saved as password_strength_chart.png")
